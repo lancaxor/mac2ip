9cef56c11fb55c51bf1ac6b37ed6ec471beeaf04
Lexis Lyashenko <alex.layt.pro@gmail.com>
2013-11-06 02:04:18 +0200

    Solve problem with defs.h

--- defs.h
+++ defs.h
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /*
  * defs.h
  *
@@ -35,30 +34,4 @@ int powr(int,int);
 void printdbg(char*);
 
 #endif /* DEFS_H_ */
-=======
-#define _AFXDLL 0
-#include <unistd.h>
-#include <stdio.h>
-#include <ctype.h>
-#include <stdlib.h>
-#include <string.h>
-//#include <WinSock2.h>
 
-#include <sys/socket.h>
-#include <arpa/inet.h>
-#include <netinet/in.h>
-
-struct in_addr sin_mac,sin_inIP,sin_outIP;
-
-long l_mac,l_out;
-
-int noFound=1;
-
-int firstParamLen=17;
-
-void dbg(long ch);
-
-void OutHelp();
-
-//int hex2dec(char*dec);
->>>>>>> e3dac8dcccfc3f678516c7b89a0af1112d4108ed
